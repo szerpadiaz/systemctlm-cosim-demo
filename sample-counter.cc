832fea7cc3503133c00a23b6db927df4f1af3165
Sergio Zerpa <szerpadiaz@gmail.com>
2019-03-07 19:39:10 +0100

    Added a new sample-counter module

---
+++ sample-counter.cc
@@ -0,0 +1,110 @@
+#define SC_INCLUDE_DYNAMIC_PROCESSES
+
+#include <inttypes.h>
+
+#include "tlm_utils/simple_initiator_socket.h"
+#include "tlm_utils/simple_target_socket.h"
+
+using namespace sc_core;
+using namespace std;
+
+#include "sample-counter.h"
+#include <sys/types.h>
+
+sCounter::sCounter(sc_module_name name)
+	: sc_module(name), t_sk("target-socket")
+{
+	t_sk.register_b_transport(this, &sCounter::b_transport);
+	m_count = 0;
+	m_ctrl = COUNTER_CTRL_RESET;
+	m_state = COUNTER_STATE_RESET;
+
+	SC_THREAD(execute);
+	sensitive << m_event_from_socket;
+}
+
+void sCounter::execute(void)
+{
+	uint8_t next;
+	while(true)
+	{
+		next = m_state;
+
+		switch(m_state)
+		{
+			case COUNTER_STATE_RESET:
+				if(m_ctrl == COUNTER_CTRL_ENABLE)
+				{
+					next = COUNTER_STATE_COUNTING;
+				}
+				break;
+			case COUNTER_STATE_COUNTING:
+				if(m_ctrl == COUNTER_CTRL_INCREMENT)
+				{
+					if(m_count < COUNTER_MAX_COUNT)
+					{
+						m_count++;
+					}
+					else
+					{
+						next = COUNTER_STATE_RESET;
+						m_count = 0;
+
+						irq.write(true);
+						wait(sc_time(1, SC_US));
+						irq.write(false);
+
+					}
+				}
+				else if(m_ctrl == COUNTER_CTRL_RESET)
+				{
+					next = COUNTER_STATE_RESET;
+					m_count = 0;
+				}
+				break;
+			default:
+				break;
+		}
+		m_state = next;
+	}
+}
+
+void sCounter::b_transport(tlm::tlm_generic_payload& transaction, sc_time& delay)
+{
+	tlm::tlm_command cmd = transaction.get_command();
+	sc_dt::uint64 addr = transaction.get_address();
+	unsigned char *data = transaction.get_data_ptr();
+	tlm::tlm_response_status status = tlm::TLM_OK_RESPONSE;
+
+	switch(cmd)
+	{
+		case (tlm::TLM_READ_COMMAND):
+			if(addr == COUNTER_REGISTER_COUNT)
+			{
+				memcpy(data, &m_count, 4);
+				m_ctrl = COUNTER_CTRL_INCREMENT;
+				m_event_from_socket.notify();
+			}
+			else
+			{
+				status = tlm::TLM_ADDRESS_ERROR_RESPONSE;
+			}
+			break;
+		case (tlm::TLM_WRITE_COMMAND):
+			if(addr == COUNTER_REGISTER_CTRL)
+			{
+				memcpy(&m_ctrl, data, 4);
+				m_event_from_socket.notify();
+			}
+			else
+			{
+				status = tlm::TLM_ADDRESS_ERROR_RESPONSE;
+			}
+			break;
+		default:
+			status = tlm::TLM_COMMAND_ERROR_RESPONSE;
+	}
+
+	transaction.set_response_status(status);
+}
+
---
+++ sample-counter.h
@@ -0,0 +1,40 @@
+
+#define COUNTER_REGISTER_CTRL  (0xFF4E0000)
+#define COUNTER_REGISTER_COUNT (0xFF4E0004)
+#define COUNTER_CTRL_RESET     (0)
+#define COUNTER_CTRL_ENABLE    (1)
+#define COUNTER_CTRL_INCREMENT (2)
+
+#define COUNTER_MAX_COUNT 	   (9)
+#define COUNTER_STATE_RESET    (0)
+#define COUNTER_STATE_COUNTING (1)
+
+/**
+ * Sample counter using TLM-sockets as input
+ */
+class sCounter
+: public sc_core::sc_module
+{
+public:
+	/**
+	 * Target socket to get the data coming from the PS.
+	 */
+	tlm_utils::simple_target_socket<sCounter> t_sk;
+
+	/**
+	 * Interrupt signal as sc-output
+	 */
+	sc_out<bool> irq;
+
+	sCounter(sc_core::sc_module_name name);
+	SC_HAS_PROCESS(sCounter);
+
+private:
+	sc_event m_event_from_socket;
+	uint32_t m_ctrl;
+	uint32_t m_count;
+	uint8_t m_state;
+
+	void execute(void);
+	virtual void b_transport(tlm::tlm_generic_payload& transaction, sc_time& delay);
+};
